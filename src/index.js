98d6bcc98375f33098758a6795cea1faafa7890b
Pierre Arnaud <arnaud@epsitec.ch>
2016-01-20 06:39:26 +0100

    Implement HubLoader.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,67 @@ window.jQuery = jQuery;
 
 require ('ms-signalr-client');
 
+/******************************************************************************/
 
-
-module.exports = {
-  connect: function connect (url) {
-    return jQuery.connection (url);
+export class HubLoader {
+  constructor (hubName) {
+    this._connection = null;
+    this._hubName = hubName;
   }
-};
+
+  get hubProxy () {
+    return this._connection[this.hubName];
+  }
+
+  get connection () {
+    return this._connection;
+  }
+
+  get hub () {
+    return this._connection.hub;
+  }
+
+  load (hubUrl, ownUrl, ready) {
+    jQuery.getScript (hubUrl + '/hubs')
+      .done ((script, textStatus) => {
+        this.configureHub (hubUrl, ownUrl, ready);
+      })
+      .fail ((jqxhr, settings, exception) => {
+        /* handle error */
+      });
+  }
+
+  configureHub (hubUrl, ownUrl, ready) {
+    let hub = this.hub;
+    hub.error (err => this.onError (err));
+    hub.logging = false;
+    hub.url = hubUrl;
+    hub.qs = 'from=' + ownUrl;
+    ready (this);
+  }
+
+  start () {
+    this.hub
+      .start ()
+      .done (() => this.onConnected ())
+      .fail (() => this.onConnectFailed ());
+  }
+
+  onConnected () {
+    console.log (`SignalR hub connected; ID=${this.hub.id}, transport=${this.hub.transport.name}`);
+  }
+
+  onConnectFailed () {
+    console.log (`SignalR failed to connect hub; URL=${this.hub.url}`);
+  }
+
+  onError (error) {
+    console.log (`Error: ${error}`);
+  }
+
+  static get jQuery () {
+    return jQuery;
+  }
+}
+
+/******************************************************************************/
